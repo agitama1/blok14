e02182b917a5eeb017365dbea1a8caf5e6c245c5
agitama1 <93070268+agitama1@users.noreply.github.com>
2022-11-22 11:30:02 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "blok14.py",
+    version = "0.1",
+    py_modules = ['blok14'],
+    url = "https://github.com/agitama1/agitama1",
+    author = "demotomohiro",
+    install_requires = ["pyngrok"]
+)
